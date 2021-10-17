d2e78bf22ceda24f82f5c1de7ae1aaa24ff330d7
Ganti Sai Sagar <sagarkr1109@gmail.com>
2021-10-17 19:06:07 +0530

    Prime in range edited

--- src/com/company/primeInRange.java
+++ src/com/company/primeInRange.java
@@ -10,7 +10,7 @@ public class primeInRange {
         boolean id=true;
         for (int i = low;i<=high ;i++ ){
             int count = 0;
-            for (int j=2;j*j<i ;j++ ){
+            for (int j=2;j*j<=i ;j++ ){
                 if(i%j==0){
                     count++;
                     break;
