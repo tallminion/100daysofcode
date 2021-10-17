531ceea271abddfe106be3f92f5b19b422a9df72
Ganti Sai Sagar <sagarkr1109@gmail.com>
2021-10-17 19:00:40 +0530

    Prime in range commit

---
+++ src/com/company/primeInRange.java
@@ -0,0 +1,24 @@
+package com.company;
+
+import java.util.Scanner;
+
+public class primeInRange {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int low = sc.nextInt();
+        int high = sc.nextInt();
+        boolean id=true;
+        for (int i = low;i<=high ;i++ ){
+            int count = 0;
+            for (int j=2;j*j<i ;j++ ){
+                if(i%j==0){
+                    count++;
+                    break;
+                }
+            }
+            if(count==0){
+                System.out.println(i);
+            }
+        }
+    }
+}
