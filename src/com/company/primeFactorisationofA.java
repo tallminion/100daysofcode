75a0ba79e489c49a4c4af53594093a583b08587b
Ganti Sai Sagar <sagarkr1109@gmail.com>
2021-10-22 23:04:51 +0530

    added question link

--- src/com/company/primeFactorisationofA.java
+++ src/com/company/primeFactorisationofA.java
@@ -1,5 +1,5 @@
 package com.company;
-
+//https://nados.pepcoding.com/content/0/9dd7eb3a-d777-42a7-a098-778cd7c89ba0/
 import java.util.Scanner;
 
 public class primeFactorisationofA {
@@ -9,8 +9,7 @@ public class primeFactorisationofA {
         for (int i = 2; i*i <= num ; i++) {
             while(num%i==0){
                 num = num/i;
-                System.out.print(i + " ");
-            }
+                System.out.print(i + " ");}
         }
         if(num!=1){
             System.out.println(num);
