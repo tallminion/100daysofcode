3ac07d69334f86a6f2fd98fd624aa168011495d8
Ganti Sai Sagar <sagarkr1109@gmail.com>
2021-10-23 22:05:30 +0530

    direct commit

---
+++ src/com/company/pattern3.java
@@ -0,0 +1,19 @@
+package com.company;
+//https://nados.pepcoding.com/content/0/075219c5-e51c-4992-bbfc-15f231083b30/
+import java.util.Scanner;
+
+public class pattern3 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        for (int i = 1; i <=n ; i++) {
+            for (int j = 5; j >=i ; j--) {
+                System.out.print(" ");
+            }
+            for (int j = 1; j <=i ; j++) {
+                System.out.print("*");
+            }
+            System.out.println("");
+        }
+    }
+}
