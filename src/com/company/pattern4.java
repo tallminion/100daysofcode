5957059bd482f95e0b471641eea455ac1e75be58
Ganti Sai Sagar <sagarkr1109@gmail.com>
2021-10-23 22:39:30 +0530

    direct commit

---
+++ src/com/company/pattern4.java
@@ -0,0 +1,24 @@
+package com.company;
+
+import java.util.Scanner;
+
+public class pattern4 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int space =0;
+        int star =n;
+        for (int i = 1; i <=n ; i++) {
+            for (int j = 1; j <=space ; j++) {
+                System.out.print(" ");
+            }
+            for (int j = 1; j <=star ; j++) {
+                System.out.print("#");
+            }
+//            System.out.println(space+ " " +star);
+            space++;
+            star--;
+            System.out.println();
+        }
+    }
+}
